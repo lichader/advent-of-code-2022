90b75337ea70621ba10a3d7c664eda9200337519
lichader <1524470+lichader@users.noreply.github.com>
2022-12-18 14:15:50 +1100

    Find the max calories

--- day1.go
+++ day1.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -18,12 +19,43 @@ func main() {
 		fmt.Println(err)
 	}
 
+	var allCalories []int
+	allCalories = append(allCalories, 0)
+	index := 0
+
 	fileScanner := bufio.NewScanner(readFile)
 	fileScanner.Split(bufio.ScanLines)
 
 	for fileScanner.Scan() {
-		fmt.Println(fileScanner.Text())
+
+		rawInput := fileScanner.Text()
+
+		if len(rawInput) == 0 {
+			fmt.Println("Encountered space. Skip")
+			index++
+			allCalories = append(allCalories, 0)
+			continue
+		}
+
+		calories, err := strconv.Atoi(rawInput)
+		if err != nil {
+			errorMessage := fmt.Sprintf("Received an error on parsing %s", rawInput)
+			fmt.Println(errorMessage)
+		}
+
+		allCalories[index] = allCalories[index] + calories
 	}
 
 	readFile.Close()
+
+	fmt.Println(allCalories)
+
+	var max int
+	for i, e := range allCalories {
+		if i == 0 || e > max {
+			max = e
+		}
+	}
+
+	fmt.Println(max)
 }
