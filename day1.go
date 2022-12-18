c66862e63dc42953339bea85bdb8770164b6fe96
lichader <1524470+lichader@users.noreply.github.com>
2022-12-18 13:33:38 +1100

    Draft of day1 file

---
+++ day1.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("No file is given in the command line")
+		return
+	}
+
+	filePath := os.Args[1]
+	readFile, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		fmt.Println(fileScanner.Text())
+	}
+
+	readFile.Close()
+}
