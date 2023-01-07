cb7ba6aaea89668e49dd12efcd19801d17928ae0
lichader <1524470+lichader@users.noreply.github.com>
2023-01-07 14:57:42 +1100

    day5: parse the intial stacks

--- day5/day5.go
+++ day5/day5.go
@@ -4,9 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 func main() {
+	moveRegex := regexp.MustCompile("^move (\\d+) from (\\d+) to (\\d+)$")
+	crateRegex := regexp.MustCompile("^\\[[A-Z]{1}\\]$")
 	const sampleFile = "./input.txt"
 	fileStream, err := os.Open(sampleFile)
 	if err != nil {
@@ -16,8 +21,41 @@ func main() {
 	fileScanner := bufio.NewScanner(fileStream)
 	fileScanner.Split(bufio.ScanLines)
 
+	var stacks [9][]string
+
 	for fileScanner.Scan() {
 		line := fileScanner.Text()
-		fmt.Println(line)
+
+		if moveRegex.MatchString(line) {
+			res := moveRegex.FindAllStringSubmatch(line, -1)
+
+			moves, _ := strconv.Atoi(res[0][1])
+			sourceCrate, _ := strconv.Atoi(res[0][2])
+			destCrate, _ := strconv.Atoi(res[0][3])
+
+			fmt.Println("Parsing line to moves: ", moves, sourceCrate, destCrate)
+		} else if strings.HasPrefix(line, "[") {
+			fmt.Println("line: ", line, " and length: ", len(line))
+
+			stackIndex := 0
+			for i := 0; i < len(line); i += 4 {
+				crate := line[i : i+3]
+
+				if crateRegex.MatchString(crate) {
+					crateName := crateRegex.FindStringSubmatch(crate)[0]
+
+					stacks[stackIndex] = append([]string{crateName}, stacks[stackIndex]...)
+				}
+
+				fmt.Println(stackIndex)
+				stackIndex++
+			}
+		} else if len(line) == 0 {
+			for i, stack := range stacks {
+				fmt.Println("index: ", i, ", stack: ", stack)
+			}
+		} else {
+			fmt.Println("Skip line: ", line)
+		}
 	}
 }
