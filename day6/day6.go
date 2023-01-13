4f1339a782d1046d5bdb5dc397b1d3fd16fd899e
lichader <1524470+lichader@users.noreply.github.com>
2023-01-13 22:52:18 +1100

    day6 part 1

---
+++ day6/day6.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const size = 4
+
+func main() {
+	sampleFile := os.Args[1]
+	fileStream, err := os.Open(sampleFile)
+	if err != nil {
+		fmt.Println("Unable to read the file", err)
+	}
+
+	fileScanner := bufio.NewScanner(fileStream)
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+
+		fmt.Println("Read line", line)
+
+		for i := 0; i <= len(line)-size; i++ {
+			subString := line[i:(i + size)]
+
+			fmt.Println(subString)
+
+			if uniqueChars(subString) {
+				fmt.Println("Index found: ", i+size)
+				break
+			}
+		}
+	}
+
+	fileStream.Close()
+}
+
+func uniqueChars(input string) bool {
+	m := make(map[rune]int)
+
+	for i, c := range input {
+		m[c] = i
+	}
+
+	if len(m) == size {
+		return true
+	}
+
+	return false
+}
